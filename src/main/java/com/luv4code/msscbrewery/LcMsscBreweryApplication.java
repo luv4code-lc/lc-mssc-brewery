c2f5797267c97f2f85f75ac6fd89cd0dd2ae79bc
luv4boot <luv4boot@gmail.com>
2021-11-30 12:08:14 +0530

    Initial Commit and Adding the BeerDto class.

---
+++ src/main/java/com/luv4code/msscbrewery/LcMsscBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.luv4code.msscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcMsscBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcMsscBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luv4code/msscbrewery/LcMsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.msscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcMsscBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
