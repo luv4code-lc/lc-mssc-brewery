e698aed3b6e4f3c77f429a2aef2e74b750de74a2
luv4boot <luv4boot@gmail.com>
2021-12-01 09:32:40 +0530

    Adding beer Api Version 2 .

--- src/main/java/com/luv4code/msscbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/com/luv4code/msscbrewery/web/controller/v2/BeerControllerV2.java
@@ -1,7 +1,7 @@
-package com.luv4code.msscbrewery.web.controller;
+package com.luv4code.msscbrewery.web.controller.v2;
 
-import com.luv4code.msscbrewery.services.BeerService;
-import com.luv4code.msscbrewery.web.model.BeerDto;
+import com.luv4code.msscbrewery.customer.services.v2.BeerServiceV2;
+import com.luv4code.msscbrewery.web.model.v2.BeerDtoV2;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,25 +10,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/beer")
-@Deprecated
-public class BeerController {
+@RequestMapping("/api/v2/beer")
+public class BeerControllerV2 {
+    private final BeerServiceV2 beerService;
 
-    private final BeerService beerService;
-
-    public BeerController(BeerService beerService) {
+    public BeerControllerV2(BeerServiceV2 beerService) {
         this.beerService = beerService;
     }
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId) {
-        BeerDto storedBeer = BeerDto.builder().build();
+    public ResponseEntity<BeerDtoV2> getBeer(@PathVariable UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<BeerDto> createBeer(@RequestBody BeerDto beerDto) {
-        BeerDto savedBeer = beerService.saveNewBeer(beerDto);
+    public ResponseEntity<BeerDtoV2> createBeer(@RequestBody BeerDtoV2 beerDto) {
+        BeerDtoV2 savedBeer = beerService.saveNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer/" + savedBeer.getId().toString());
@@ -36,7 +33,7 @@ public class BeerController {
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity handleUpdate(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity handleUpdate(@PathVariable UUID beerId, @RequestBody BeerDtoV2 beerDto) {
         beerService.updateBeer(beerId, beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
@@ -46,5 +43,4 @@ public class BeerController {
     public void deleteBeer(@PathVariable UUID beerId) {
         beerService.deleteBeerById(beerId);
     }
-
 }
---
+++ src/main/java/com/luv4code/msscbrewery/web/model/v2/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.luv4code.msscbrewery.web.model.v2;
+
+public enum BeerStyleEnum {
+    LASER, PISLSNER, ALE, STOUT, GOSE, IPA
+}
