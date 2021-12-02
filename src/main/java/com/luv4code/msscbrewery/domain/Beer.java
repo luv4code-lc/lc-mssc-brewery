19ff683bd16e319c1e88664a347090bb421417e0
luv4code-lfc <luv4code.lc@gmail.com>
2021-12-02 08:58:50 +0530

    Adding Mapper interface to create Mapper Implementaion class

---
+++ src/main/java/com/luv4code/msscbrewery/domain/Beer.java
@@ -0,0 +1,20 @@
+package com.luv4code.msscbrewery.domain;
+
+import com.luv4code.msscbrewery.web.model.v2.BeerStyleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+    private UUID id;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+}
---
+++ src/main/java/com/luv4code/msscbrewery/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package com.luv4code.msscbrewery.mappers;
+
+import com.luv4code.msscbrewery.domain.Beer;
+import com.luv4code.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
